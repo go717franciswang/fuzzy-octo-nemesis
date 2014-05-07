40fe39e762dc339adc3fdad0e3d04cba1624d415
Francis Wang <go717franciswang@hotmail.com>
2014-05-07 12:29:14 +0800

    Test openmp

---
+++ homework4/test2.f90
@@ -0,0 +1,16 @@
+program test
+    use omp_lib
+    integer :: thread_num
+
+    !$ call omp_set_num_threads(2)
+
+    print *, "Testing openmp..."
+
+    !$omp parallel
+    !$omp critical
+    !$ thread_num = omp_get_thread_num()
+    !$ print *, "This thread = ", thread_num
+    !$omp end critical
+    !$omp end parallel
+
+end program test
