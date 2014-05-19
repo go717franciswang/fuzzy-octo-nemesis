353a0147267b63f478095c8da0f52f96d0933d95
Francis Wang <francis@bposolutions.com>
2014-05-20 06:08:16 +0800

    Complete hw4p3

--- homework4/test2.f90
+++ homework4/test2.f90
@@ -1,16 +1,34 @@
-program test
-    use omp_lib
-    integer :: thread_num
 
-    !$ call omp_set_num_threads(2)
+program test2
 
-    print *, "Testing openmp..."
+    use quadrature, only: trapezoid, error_table
 
-    !$omp parallel
-    !$omp critical
-    !$ thread_num = omp_get_thread_num()
-    !$ print *, "This thread = ", thread_num
-    !$omp end critical
-    !$omp end parallel
+    implicit none
+    real(kind=8) :: a,b,int_true
+    integer :: nvals(12), i
 
-end program test
+    a = 0.d0
+    b = 2.d0
+    int_true = b + b**4/4. + cos(1000.*b)/1000.
+
+    print 10, int_true
+ 10 format("true integral: ", es22.14)
+    print *, " "  ! blank line
+
+    ! values of n to test:
+    do i=1,12
+        nvals(i) = 5 * 2**(i-1)
+        enddo
+
+    call error_table(f, a, b, nvals, int_true)
+
+contains
+
+    real(kind=8) function f(x)
+        implicit none
+        real(kind=8), intent(in) :: x 
+        
+        f = 1. + x**3 + sin(1000. * x)
+    end function f
+
+end program test2
