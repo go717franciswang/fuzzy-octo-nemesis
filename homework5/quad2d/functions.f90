b5a22cf56cde2b5a5f3c96b5eaeb6504cd84e3a2
Francis Wang <francis@bposolutions.com>
2014-06-13 10:42:33 -0700

    Complete hw5

--- homework5/quad2d/functions.f90
+++ homework5/quad2d/functions.f90
@@ -4,6 +4,7 @@ module functions
     use omp_lib
     implicit none
     integer :: fevals(0:7)
+    integer :: gevals(0:7)
     real(kind=8) :: k
     save
 
@@ -12,7 +13,9 @@ contains
     real(kind=8) function f(x)
         implicit none
         real(kind=8), intent(in) :: x 
-        integer thread_num
+
+        real(kind=8) :: a,b,h,y
+        integer :: n,j,thread_num
 
         ! keep track of number of function evaluations by
         ! each thread:
@@ -20,8 +23,29 @@ contains
         !$ thread_num = omp_get_thread_num()
         fevals(thread_num) = fevals(thread_num) + 1
         
-        f = 1.d0 + x**3 + sin(k*x)
+        a = 1
+        b = 4
+        n = 1000
+        h = (b-a)/(n-1)
+        f = 0.5d0*(g(x,a) + g(x,b))
+        do j=2,n-1
+            y = a + (j-1)*h
+            f = f + g(x,y)
+            enddo
+        f = h * f
         
     end function f
+
+    real(kind=8) function g(x, y)
+        implicit none
+        real(kind=8), intent(in) :: x,y
+        integer :: thread_num
+
+        thread_num = 0
+        !$ thread_num = omp_get_thread_num()
+        gevals(thread_num) = gevals(thread_num) + 1
+
+        g = sin(x + y)
+    end function g
 
 end module functions
