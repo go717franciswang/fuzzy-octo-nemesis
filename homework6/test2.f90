d40b65c5f5ef4c24d333b02bdc1fc58d4cba8c2d
Francis Wang <francis@bposolutions.com>
2014-06-19 13:09:39 -0700

    Use master slave paradigm

--- homework6/test2.f90
+++ homework6/test2.f90
@@ -7,9 +7,10 @@ program test2
     use functions, only: f, fevals_proc, k
 
     implicit none
-    real(kind=8) :: a,b,int_true, int_approx, h, int_approx_total
+    real(kind=8) :: a,b,int_true, int_approx, h, int_approx_total, dx_sub
+    real(kind=8), dimension(2) :: ab_sub
 
-    integer :: proc_num, num_procs, ierr, n, fevals_total
+    integer :: proc_num, num_procs, ierr, n, fevals_total, j
     integer, dimension(MPI_STATUS_SIZE) :: status
 
     call MPI_INIT(ierr)
@@ -30,31 +31,58 @@ program test2
         print '("Using ",i3," processes")', num_procs
         print '("true integral: ", es22.14)', int_true
         print *, " "  ! blank line
-        endif
+    endif
 
     call MPI_BARRIER(MPI_COMM_WORLD,ierr) ! wait for process 0 to print
 
-    ! Note: In this version all processes call trap and repeat the
-    !       same work, so each should get the same answer.  
-    h = (b-a)/num_procs
-    int_approx = trapezoid(f,a+h*proc_num,a+h*(proc_num+1),n/num_procs)
+    ! -------------------------
+    ! Master
+    ! -------------------------
+    if (proc_num == 0) then
 
-    call MPI_BARRIER(MPI_COMM_WORLD,ierr) ! wait for all process to print
+        dx_sub = (b-a) / (num_procs-1)
+        do j = 1,num_procs-1
+            ab_sub(1) = a + (j-1)*dx_sub
+            ab_sub(2) = a + j*dx_sub
+            call MPI_SEND(ab_sub, 2, MPI_DOUBLE_PRECISION, j, j, &
+                MPI_COMM_WORLD, ierr)
+        enddo
 
-    ! print the number of function evaluations by each thread:
+        do j = 1,num_procs-1
+            call MPI_RECV(int_approx, 1, MPI_DOUBLE_PRECISION, &
+                MPI_ANY_SOURCE, MPI_ANY_TAG, &
+                MPI_COMM_WORLD, status, ierr)
+            int_approx_total = int_approx_total + int_approx
+        enddo
+    endif
+
+    ! -------------------------
+    ! Slave
+    ! -------------------------
+    if (proc_num /= 0) then
+        call MPI_RECV(ab_sub, 2, MPI_DOUBLE_PRECISION, &
+            0, proc_num, &
+            MPI_COMM_WORLD, status, ierr)
+        int_approx = trapezoid(f,ab_sub(1),ab_sub(2),n)
+
+        call MPI_SEND(int_approx, 1, MPI_DOUBLE_PRECISION, 0, proc_num, &
+            MPI_COMM_WORLD, ierr)
+
+        print '("Process ", i2, " with n = ", i8, " computes int_approx = ", es22.14)', &
+            proc_num, n, int_approx
+    endif
+
+    call MPI_BARRIER(MPI_COMM_WORLD,ierr)
     print '("fevals by Process ",i2,": ",i13)',  proc_num, fevals_proc
-
-    call MPI_BARRIER(MPI_COMM_WORLD,ierr) ! wait for all process to print
     call MPI_REDUCE(fevals_proc, fevals_total, 1, MPI_INTEGER, MPI_SUM, 0, &
                     MPI_COMM_WORLD, ierr)
-    call MPI_REDUCE(int_approx, int_approx_total, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, &
-                    MPI_COMM_WORLD, ierr)
 
-    if (proc_num==0) then
+    if (proc_num == 0) then
         print '("Total number of fevals: ",i10)', fevals_total
         print '("int_approx = ",es22.14)', int_approx_total
-        endif
+    endif
 
+    call MPI_BARRIER(MPI_COMM_WORLD,ierr)
     call MPI_FINALIZE(ierr)
 
 end program test2
