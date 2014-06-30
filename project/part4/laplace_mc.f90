41f9d39f9f79433078221dd0952ba99f1f9ca306
Francis Wang <francis@bposolutions.com>
2014-06-30 16:33:00 -0700

    Complete finalP4

--- project/part4/laplace_mc.f90
+++ project/part4/laplace_mc.f90
@@ -1,11 +1,17 @@
 program laplace_mc
+    use mpi
     use problem_description, only: ax,ay,dx,dy,utrue,nx,ny
     use random_util, only: init_random_seed
-    use mc_walk, only: many_walks
+    use mc_walk, only: many_walks, nwalks
 
     implicit none
     real(kind=8) :: x0, y0, u_mc, u_mc_total, u_sum_new, u_sum_old, u_true, error
-    integer :: i0, j0, maxsteps, seed, n_mc, n_success, n_total, i
+    integer :: i0, j0, maxsteps, seed, n_mc, n_success, n_total, i, ierr, proc_num, num_procs, &
+        nwalks_total
+
+    call MPI_INIT(ierr)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, proc_num, ierr)
 
     x0 = 0.9
     y0 = 0.6
@@ -17,15 +23,20 @@ program laplace_mc
 
     u_true = utrue(x0, y0)
 
-    print 10, x0, y0, u_true
-10  format ("True solution of PDE: u(",es10.3,", ",es10.3,") = ",es11.5)
-    print *, "Note: with solution used in demo this is also the solution to the"
-    print *, "      finite-difference equations on the same grid."
+    if (proc_num == 0) then
+        print 10, x0, y0, u_true
+    10  format ("True solution of PDE: u(",es10.3,", ",es10.3,") = ",es11.5)
+        print *, "Note: with solution used in demo this is also the solution to the"
+        print *, "      finite-difference equations on the same grid."
 
-    print *, "Enter seed for random generator or <return> .... "
-    read *, seed
+        print *, "Enter seed for random generator or <return> .... "
+        read *, seed
+    endif
+    call MPI_BCAST(seed, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
 
+    seed = seed + 97*proc_num ! unique for each process
     call init_random_seed(seed)
+    call MPI_BARRIER(MPI_COMM_WORLD, ierr)
     maxsteps = 100*max(nx, ny)
     n_mc = 10
     call many_walks(i0, j0, maxsteps, n_mc, u_mc, n_success)
@@ -33,7 +44,9 @@ program laplace_mc
     u_mc_total = u_mc
     n_total = n_success
 
-    open(unit=25, file='laplace_mc_error.txt', status='unknown')
+    if (proc_num == 0) then
+        open(unit=25, file='laplace_mc_error.txt', status='unknown')
+    endif
 
     do i = 1,12
         u_sum_old = u_mc_total * n_total
@@ -43,12 +56,23 @@ program laplace_mc
         u_mc_total = (u_sum_old + u_sum_new) / n_total
         error = abs((u_mc_total - u_true) / u_true)
 
-        print '("After ",i8," random walks, u = ",es15.9,", ref. error = ",es15.6)', &
-            n_total, u_mc_total, error
-        write(25, *) n_total, u_mc_total, error
-!11      format (i10,e23.15,e15.6)
+        if (proc_num == 0) then
+            print '("After ",i8," random walks, u = ",es15.9,", ref. error = ",es15.6)', &
+                n_total, u_mc_total, error
+            write(25, *) n_total, u_mc_total, error
+        endif
 
         n_mc = 2*n_mc
     enddo
+    call MPI_REDUCE(nwalks, nwalks_total, 1, MPI_INTEGER, MPI_SUM, 0, &
+        MPI_COMM_WORLD, ierr)
+    if (proc_num == 0) then
+        print *, "Total walks performed by all processes: ", nwalks_total
+    endif
+    call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+
+    print *, 'Wlkas performed by Process ', proc_num, ': ', nwalks
+
+    call MPI_FINALIZE(ierr)
 
 endprogram laplace_mc
--- project/part4/mc_walk.f90
+++ project/part4/mc_walk.f90
@@ -1,6 +1,9 @@
 module mc_walk
+    use mpi
     use problem_description, only: ax, bx, ay, by, uboundary, nx, ny, dx, dy
     implicit none
+
+    integer :: nwalks
 
 contains
 
@@ -16,6 +19,7 @@ subroutine random_walk(i0, j0, max_steps, ub, iabort)
     i = i0
     j = j0
     iabort = 0
+    nwalks = nwalks + 1
 
     call random_number(r)
     do k = 1,max_steps
@@ -40,6 +44,7 @@ subroutine random_walk(i0, j0, max_steps, ub, iabort)
             iabort = 1
         endif
     enddo
+
 endsubroutine
 
 subroutine many_walks(i0, j0, max_steps, n_mc, u_mc, n_success)
@@ -47,21 +52,61 @@ subroutine many_walks(i0, j0, max_steps, n_mc, u_mc, n_success)
     real(kind=8), intent(out) :: u_mc
     integer, intent(out) :: n_success
 
-    integer :: k, i, j, iabort
+    integer :: k, iabort, ierr, num_procs, proc_num, numsent, sender
     real(kind=8) :: ub, ub_sum
+    integer, dimension(MPI_STATUS_SIZE) :: status
+
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, proc_num, ierr)
 
     n_success = 0
     ub_sum = 0
+    numsent = 0
 
-    do k=1,n_mc
-        call random_walk(i0, j0, max_steps, ub, iabort)
-        if (iabort == 0) then
-            ub_sum = ub_sum + ub
-            n_success = n_success + 1
-        endif
-    enddo
+    if (proc_num == 0) then
+        do k=1,min(n_mc, num_procs-1)
+            call MPI_SEND(MPI_BOTTOM, 0, MPI_INTEGER, k, 1, &
+                MPI_COMM_WORLD, ierr)
+            numsent = numsent + 1
+        enddo
+
+        do k=1,n_mc
+            call MPI_RECV(ub, 1, MPI_DOUBLE_PRECISION, &
+                MPI_ANY_SOURCE, MPI_ANY_TAG, &
+                MPI_COMM_WORLD, status, ierr)
+            sender = status(MPI_SOURCE)
+            iabort = status(MPI_TAG)
+            if (iabort == 0) then
+                ub_sum = ub_sum + ub
+                n_success = n_success + 1
+            endif
+
+            if (numsent < n_mc) then
+                call MPI_SEND(MPI_BOTTOM, 0, MPI_INTEGER, sender, 1, &
+                    MPI_COMM_WORLD, ierr)
+                numsent = numsent + 1
+            else
+                call MPI_SEND(MPI_BOTTOM, 0, MPI_INTEGER, sender, 0, &
+                    MPI_COMM_WORLD, ierr)
+            endif
+        enddo
+    else
+        do while (.true.)
+            call MPI_RECV(MPI_BOTTOM, 0, MPI_INTEGER, &
+                0, MPI_ANY_TAG, MPI_COMM_WORLD, status, ierr)
+            k = status(MPI_TAG)
+            if (k == 0) goto 99
+
+            call random_walk(i0, j0, max_steps, ub, iabort)
+            call MPI_SEND(ub, 1, MPI_DOUBLE_PRECISION, 0, &
+                iabort, MPI_COMM_WORLD, ierr)
+        enddo
+    endif
 
     u_mc = ub_sum / n_success
+
+99  continue
+    call MPI_BARRIER(MPI_COMM_WORLD, ierr)
 endsubroutine many_walks
 
 endmodule mc_walk
