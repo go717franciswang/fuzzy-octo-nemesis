8cdeceef3bb63eafcf413e99e0d128b597f033d4
Francis Wang <francis@bposolutions.com>
2014-06-30 12:12:27 -0700

    Complete finalP3

--- project/part3/laplace_mc.f90
+++ project/part3/laplace_mc.f90
@@ -1,39 +1,41 @@
 program laplace_mc
-    use problem_description, only: ax,ay,dx,dy,utrue
+    use problem_description, only: ax,ay,dx,dy,utrue,nx,ny
     use random_util, only: init_random_seed
+    use mc_walk, only: many_walks
 
     implicit none
-    real(kind=8) :: x0, y0, u_mc, u_mc_total, u_sum_new, u_sum_old
+    real(kind=8) :: x0, y0, u_mc, u_mc_total, u_sum_new, u_sum_old, u_true, error
     integer :: i0, j0, maxsteps, seed, n_mc, n_success, n_total, i
 
     x0 = 0.9
     y0 = 0.6
-    i0 = round((x0-ax)/dx)
-    j0 = round((y0-ay)/dy)
+    i0 = nint((x0-ax)/dx)
+    j0 = nint((y0-ay)/dy)
 
     x0 = ax + i0*dx
     y0 = ay + j0*dy
 
     u_true = utrue(x0, y0)
 
-    print '("True solution of PDE: u(",es7.3,", ",es7.3,") = ",es9.5)', x0, y0, u_true
-    print "Note: with solution used in demo this is also the solution to the"
-    print "      finite-difference equations on the same grid."
+    print 10, x0, y0, u_true
+10  format ("True solution of PDE: u(",es10.3,", ",es10.3,") = ",es11.5)
+    print *, "Note: with solution used in demo this is also the solution to the"
+    print *, "      finite-difference equations on the same grid."
 
-    print "Enter seed for random generator or <return> .... "
+    print *, "Enter seed for random generator or <return> .... "
     read *, seed
 
-    init_random_seed(seed)
+    call init_random_seed(seed)
     maxsteps = 100*max(nx, ny)
     n_mc = 10
-    many_walks(i0, j0, maxsteps, n_mc, u_mc, n_success)
+    call many_walks(i0, j0, maxsteps, n_mc, u_mc, n_success)
 
     u_mc_total = u_mc
     n_total = n_success
 
     do i = 1,12
         u_sum_old = u_mc_total * n_total
-        many_walks(i0, j0, maxsteps, n_mc, u_mc, n_success)
+        call many_walks(i0, j0, maxsteps, n_mc, u_mc, n_success)
         u_sum_new = u_mc * n_success
         n_total = n_total + n_success
         u_mc_total = (u_sum_old + u_sum_new) / n_total
--- project/part3/mc_walk.f90
+++ project/part3/mc_walk.f90
@@ -7,35 +7,36 @@ contains
 subroutine random_walk(i0, j0, max_steps, ub, iabort)
     integer, intent(in) :: i0, j0, max_steps
     real(kind=8), intent(out) :: ub
-    integer, integer(out) :: iabort
+    integer, intent(out) :: iabort
 
     real(kind=4) :: r(max_steps)
-    integer :: i, j
+    real(kind=8) :: xb, yb
+    integer :: i, j, k
 
     i = i0
     j = j0
     iabort = 0
 
     call random_number(r)
-    do i = 1,max_steps
-        if r(i) < 0.25 then
+    do k = 1,max_steps
+        if (r(k) < 0.25) then
             i = i-1
-        elseif r(i) < 0.5 then
+        elseif (r(k) < 0.5) then
             i = i+1
-        elseif r(i) < 0.75 then
+        elseif (r(k) < 0.75) then
             j = j-1
         else
             j = j+1
         endif
 
-        if i*j*(nx+1-i)*(ny+1-j) == 0 then
+        if (i*j*(nx+1-i)*(ny+1-j) == 0) then
             xb = ax + i*dx
             yb = ay + j*dy
             ub = uboundary(xb, yb)
             return
         endif
 
-        if i == max_steps then
+        if (k == max_steps) then
             iabort = 1
         endif
     enddo
@@ -46,14 +47,15 @@ subroutine many_walks(i0, j0, max_steps, n_mc, u_mc, n_success)
     real(kind=8), intent(out) :: u_mc
     integer, intent(out) :: n_success
 
-    integer :: k, i, j
-    real(kind=8) ;; ub, ub_sum
+    integer :: k, i, j, iabort
+    real(kind=8) :: ub, ub_sum
 
     n_success = 0
+    ub_sum = 0
 
     do k=1,n_mc
-        random_walk(i0, j0, max_steps, ub, iabort)
-        if iabort == 0 then
+        call random_walk(i0, j0, max_steps, ub, iabort)
+        if (iabort == 0) then
             ub_sum = ub_sum + ub
             n_success = n_success + 1
         endif
